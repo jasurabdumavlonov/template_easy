2efde1a1553e904a66a7ee97643d285fb1f93772
JR | Frontend Engineer <jasurackerman2004@gmail.com>
2024-01-06 09:36:23 +0500

    full project of easy template

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a = 10
+
+let b = 20
+
+let c = 30
+
+
+if (a > b && a > c) {
+  console.log("Hello world!");
+} else if (b > a && b > c) {
+  console.log("Bye world!")
+} else {
+  console.log("Welcome world")
+}
+
